1df2030cbb0be0c9bf4b0e2a0e96d59051a981cf
Jasper Robinson <jrobi001@gold.ac.uk>
2020-02-15 18:20:41 +0000

    initialised MySQL in project to use local copy of database: shelfdatav1

--- ServerDev/app.js
+++ ServerDev/app.js
@@ -5,8 +5,30 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+//to do: install mysql
+var mysql = require('mysql');
 
 const app = express();
+
+
+//replace password with your own.
+// may need to execute two queries in MySQL workbench for server to work:
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+// flush privileges;
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'cake123',
+    database: 'shelfdatav1'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to the database');
+});
+global.db = db;
 
 //more routes files can be added - generally advised to have one for each group of pages
 const routes = require('./routes/routes')
