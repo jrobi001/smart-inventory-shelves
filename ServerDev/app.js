8bb3bc474cb5e6754089c2eacaefcd1749beccec
Jasper Robinson <jrobi001@gold.ac.uk>
2020-02-22 21:03:54 +0000

    changed folder structure to reflect MVC

--- ServerDev/app.js
+++ ServerDev/app.js
@@ -1,49 +1,27 @@
-// nodemon is installed for live updating - to start the server (with nodemon)
-// run 'npm start'
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//to do: install mysql
-var mysql = require('mysql');
+
+const db = require('../util/database');
+const errorController = require('./controllers/error');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-//replace password with your own.
-// may need to execute two queries in MySQL workbench for server to work:
-// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-// flush privileges;
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'cake123',
-    database: 'shelfdatav1'
-});
-
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to the database');
-});
-global.db = db;
-
-//more routes files can be added - generally advised to have one for each group of pages
 const routes = require('./routes/routes')
+const testRoutes = require('./routes/testRoutes')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/test', testRoutes);
 app.use(routes);
 
-//route for the unknown domain at the bottom of all routing.
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
 
+app.use(errorController.get404);
 
-// Can access the site throught localhost 3000
-// on windows: http://localhost:3000/
+// http://localhost:3000/
 app.listen(3000);
--- ServerDev/routes/routes.js
+++ ServerDev/routes/routes.js
@@ -7,7 +7,6 @@ const rootDir = require('../util/path')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    // console.log('In another Middleware');
     res.sendFile(path.join(rootDir, 'views', 'main.html'))
 });
 
