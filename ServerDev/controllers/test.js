5333f924c382a9242e30c636f69d83dc40113c17
Jasper Robinson <jrobi001@gold.ac.uk>
2020-02-23 14:30:54 +0000

    add item form works, created css grid for shelf overview list

--- ServerDev/controllers/test.js
+++ ServerDev/controllers/test.js
@@ -1,21 +1,34 @@
-exports.getAddItem = (req, res, next) => {
-    res.render('test/add-item-form'), {
-        pageTitle: 'Add Item'
-    }
+const Item = require('../models/item')
 
+exports.getAddItem = (req, res, next) => {
+    res.render('test/add-item-form', {
+        pageTitle: 'Add Item'
+    });
 }
 
 exports.postAddItem = (req, res, next) => {
     const itemName = req.body.name;
     const tags = req.body.tags;
+    const weight = req.body.weight;
     const notes = req.body.notes;
     const price = req.body.price;
-    const weight = req.body.weight;
-    //call model method to insert data to db here
-    console.log(itemName);
-    console.log(tags);
-    console.log(notes);
-    console.log(price);
-    console.log(weight);
-    res.redirect('/');
+    const imageLink = req.body.imageLink;
+
+    const newItem = new Item(null, itemName, tags, weight, notes, price, imageLink);
+    newItem.addItem()
+        .then(() => {
+            console.log('success new item ' + itemName + ' has been added')
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
+}
+
+exports.getShelfOverview = (req, res, next) => {
+    res.render('test/overview-list', {
+        pageTitle: 'Shelf Overview List'
+    });
+}
+
+exports.getSetUpShelf = (req, res, next) => {
+
 }
