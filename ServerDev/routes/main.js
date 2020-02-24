a1a3cabb2c0ad4f49b6d96dce01589273f306ec0
Jasper Robinson <jrobi001@gold.ac.uk>
2020-02-24 22:14:34 +0000

    moved route / to main.js, fixed one small mistake

--- ServerDev/routes/main.js
+++ ServerDev/routes/main.js
@@ -3,6 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 //routes can be placed here as in data web if wanted
+router.get('/', (req, res, next) => {
+    let sqlQuery = "SELECT * FROM shelves";
+    db.query(sqlQuery, (err, result) => {
+        if (err) {
+            res.redirect('./');
+        }
+        console.log(result);
+        res.render('main');
+    });
 
+});
 
 module.exports = router
--- ServerDev/routes/routes.js
+++ ServerDev/routes/routes.js
@@ -2,16 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    let sqlQuery = "SELECT * FROM shelves";
-    db.query(sqlQuery, (err, result) => {
-        if (err) {
-            res.redirect('./');
-        }
-        console.log(result);
-        res.render('main');
-    });
 
-});
 
 module.exports = router
