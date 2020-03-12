a3e085407a5e97af5865f7ba7b80d4095b1a7cc7
Jignesh <jdevj001@gold.ac.uk>
2020-03-12 15:04:46 +0000

    added the auto calculate 100 percent weight function

--- ServerDev/routes/main.js
+++ ServerDev/routes/main.js
@@ -26,9 +26,79 @@ router.get('/', (req, res, next) => {
 router.get('/404', function (req, res) {
     res.render('404.ejs', { pageTitle: '404 ERROR' });
 
-})
+});
+
+router.get('/edit-shelf-details', function (req,res){
+	res.render('edit-shelf-details.ejs',{pageTitle:'Edit Shelf Details'});
+});
 
 
+
+
+function autoCalcweight(){
+let sqlqueryinit = "select shelfPosition from shelves where items_id != 'null' and autocalc100Percent = '1'";
+db.query(sqlqueryinit, (err,result) =>{
+	
+if (result[0] != undefined){
+var i;
+for( i=0; i<result.length; i++){
+ let shelfPos = result[i].shelfPosition;
+ 
+	
+	
+
+let sqlquery0 = "select hundredPercent from shelves where shelfPosition = ?";
+let record0 = [shelfPos];
+	db.query(sqlquery0, record0, (err1,result1) =>{
+		if (err1){throw err1}
+		let currentcalibratedWeight = result1[0].hundredPercent;
+			
+if(record0 == '1'){	
+var sqlquery1 = "select weight from id1weights order by dateTime desc limit 1";
+}
+if(record0 == '2'){
+var sqlquery1 = "select weight from id2weights order by dateTime desc limit 1";
+}
+if(record0 == '3'){
+var sqlquery1 = "select weight from id3weights order by dateTime desc limit 1";
+}
+if(record0 == '4'){
+var sqlquery1 = "select weight from id4weights order by dateTime desc limit 1";
+}
+if(record0 == '5'){
+var sqlquery1 = "select weight from id5weights order by dateTime desc limit 1";
+}
+if(record0 == '6'){
+var sqlquery1 = "select weight from id6weights order by dateTime desc limit 1";
+}
+db.query(sqlquery1, (err2, result2) => {
+	if (err2) {throw err2}
+	
+	if(result2[0]!= undefined){
+	let weightval = result2[0].weight;
+	
+	if(weightval>currentcalibratedWeight){
+	let sqlquery = "update shelves set hundredPercent = ? where shelfPosition = ?";
+	let record1 = [weightval,shelfPos];	
+	db.query(sqlquery, record1, (err3, result3) => {
+	if(err3){throw err3}
+	
+      });
+    }
+  }
+});
+});
+
+}
+}
+});
+}
+
+autoCalcweight();
+
+setInterval(function(){
+    autoCalcweight()
+}, 30000)
 // Logic for delete-----------------------------------------------------------------------
 // moved to delete.js
 //updated to also delete all id?weights records from the relevant table
@@ -63,7 +133,9 @@ router.get('/404', function (req, res) {
 //removed selector by typing name, used shelfpos instead
 
 // router.get('/item-select', function (req, res) {
-//     res.render('select-item.ejs', { pageTitle: 'Item Select' });
+//     res.render('selsetInterval(function(){
+   // myFunction()
+//}, 30000)ect-item.ejs', { pageTitle: 'Item Select' });
 
 // });
 
