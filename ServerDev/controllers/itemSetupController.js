a6340f89601f2491363960c13f36f2f996073cf5
Jasper Robinson <jrobi001@gold.ac.uk>
2020-03-15 21:34:51 +0000

    started work on sorting main page by position, percent or absolute weight, but finishing work for the evening

--- ServerDev/controllers/itemSetupController.js
+++ ServerDev/controllers/itemSetupController.js
@@ -18,12 +18,10 @@ exports.getShefSelector = (req, res, next) => {
 }
 
 exports.postConfirmShelf = (req, res, next) => {
-    // console.log('hello');
     const shelfPosition = req.body.shelfPos;
     // console.log(shelfPosition)
     Shelf.fetchItemIdFromPos(shelfPosition)
         .then(([data, meta]) => {
-            // console.log('still working');
             const itemId = data[0].items_id
             if (itemId == null) {
                 return res.render('item-setup/item-form', {
@@ -35,7 +33,7 @@ exports.postConfirmShelf = (req, res, next) => {
             }
         }).then(([data, meta]) => {
             const itemData = data[0];
-            console.log(itemData.name);
+            // console.log(itemData.name);
             res.render('item-setup/confirm-shelf', {
                 pageTitle: 'Confirm shelf',
                 shelfPos: shelfPosition,
@@ -108,55 +106,4 @@ exports.postSetupComplete = (req, res, next) => {
             })
         })
         .catch(err => console.log(err));
-
 }
-
-// alt edit function not needed-----------------------------------------------------------------------
-// exports.getEditItem = (req, res, next) => {
-//     const shelfPos = req.params.shelfPos;
-//     Shelf.fetchItemIdFromPos(shelfPos)
-//         .then(([data, meta]) => {
-//             const itemId = data[0].items_id;
-//             console.log(itemId)
-//             if (itemId == null) {
-//                 res.send('that shelf is empty, maybe we will redirect to the add item page hehre!')
-//             }
-//             return Item.findById(itemId);
-//         })
-//         .then(([data, meta]) => {
-//             item = data[0];
-//             // console.log(item);
-//             res.render('item-setup/edit-item', {
-//                 pageTitle: 'edit item',
-//                 shelfPos: shelfPos,
-//                 itemId: item.id,
-//                 itemName: item.name,
-//                 itemTags: item.tags,
-//                 itemWeight: item.weight,
-//                 itemNotes: item.notes,
-//                 itemPrice: item.price,
-//                 itemImageLink: item.imageLink
-//             });
-//         })
-//         .catch(err => console.log(err));
-// }
-
-// exports.postEditConfirmed = (req, res, next) => {
-//     const itemName = req.body.name;
-//     const tags = req.body.tags;
-//     const weight = req.body.weight;
-//     const notes = req.body.notes;
-//     const price = req.body.price;
-//     const imageLink = req.body.imageLink;
-//     const itemId = parseInt(req.body.itemId);
-//     const shelfPos = req.body.shelfPos;
-
-
-//     const editedItem = new Item(itemId, itemName, tags, weight, notes, price, imageLink);
-//     // console.log(editedItem);
-//     editedItem.updateItem()
-//         .then(
-//             res.send('congrats the item ' + itemName + ' on shelf ' + itemId + 'has been updated')
-//         )
-//         .catch(err => console.log(err));
-// }
